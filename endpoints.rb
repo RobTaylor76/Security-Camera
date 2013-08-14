ab41135018f79aa5200da45fa2690005b56fdf9c
Rob Taylor <robert_taylor_@hotmail.com>
2013-08-14 19:52:09 +0000

    ignore any errors!!! just prototyping at the mo

--- endpoints.rb
+++ endpoints.rb
@@ -5,7 +5,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 
 get '/alarm' do
+  begin
   command = 'ruby file_download.rb&'
   system(command)
+  rescue
+  end
   "Hello Sinatra!"
 end
--- file_download.rb
+++ file_download.rb
@@ -4,7 +4,10 @@ require 'open-uri'
 10.times do
   filename = Time.now.strftime("%Y_%m_%d_%H%M%S")
   open("images/#{filename}.jpg", 'wb') do |file|
+    begin
     file << open('http://192.168.1.253:81/snapshot.cgi?user=GUEST&password=9999').read
+    rescue
+    end
   end
   sleep 1
 end
