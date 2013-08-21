0bf27f6edf75995c7d4003ff4be1a0ad1389307d
Rob Taylor <robert_taylor_@hotmail.com>
2013-08-21 20:36:20 +0100

    added force option for camera and fixed timeboxing

--- take_pictures.rb
+++ take_pictures.rb
@@ -1,16 +1,22 @@
 DATA.flock(File::LOCK_EX | File::LOCK_NB) or abort 'Already Running'
 time = Time.now
-exit if time.hour > 20 || time.hour < 7 #time box pictures to daylight
+force = false
+ARGV.each do|a|
+  force = true if a == '--force'
+end
+unless force
+  exit if (time.hour >= 20) || (time.hour < 7) #time box pictures to daylight
+end
 date = time.strftime("%Y_%m_%d")
 directory = "images/#{date}"
 unless Dir.exists? directory
   Dir.mkdir directory
 end
-10.times do
+3.times do
   timestamp = Time.now.strftime("%H_%M__%S")
   filename = "#{directory}/#{timestamp}.jpg"
   begin
-    command = "raspistill -o #{filename} -t 1 -vf -q 50"
+    command = "raspistill -o #{filename} -t 1 -vf -hf -q 50"
     system(command)
   rescue
   end
